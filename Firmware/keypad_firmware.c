ceb17064c94bf70b54bd4ff18c956ed6d72fbe46
waynepadgett <wayne.padgett@rose-hulman.edu>
2025-11-28 18:07:07 -0500

    Implement Phase 2: Integrate USB Keypad HAL into firmware

--- Firmware/keypad_firmware.c
+++ Firmware/keypad_firmware.c
@@ -1,64 +1,84 @@
 /* This code is for the keypad firmware for the Hampod Program
 * Written by Brendan Perez
 * Last modified on 10/23/2023
+* Updated for Raspberry Pi USB HAL on 11/28/2025
 */
+#ifndef KEYPAD_CODE
+#define KEYPAD_CODE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#include "hampod_queue.h"
+#include "hampod_firm_packet.h"
+#include "hal/hal_keypad.h"
+
+#define KEYPAD_O "Keypad_o"
+#define KEYPAD_I "Keypad_i"
+
+#define KEYPAD_THREAD_COLOR "\033[0;97mKeypad - Main: "
+#define KEYPAD_IO_THREAD_COLOR "\033[0;96mKeypad - IO: "
+
+#ifdef DEBUG
+#define KEYPAD_PRINTF(...) \
+    do { \
+        if(DEBUG) { \
+            printf(KEYPAD_THREAD_COLOR); \
+            printf(__VA_ARGS__); \
+        } \
+    } while(0)
+
+#define KEYPAD_IO_PRINTF(...) \
+    do { \
+        if(DEBUG) { \
+            printf(KEYPAD_IO_THREAD_COLOR); \
+            printf(__VA_ARGS__); \
+        } \
+    } while(0)
+#else
+
+#define KEYPAD_PRINTF(...) \
+    while(0)
+
+#define KEYPAD_IO_PRINTF(...) \
+    while(0)
+
+#endif
+
+typedef struct keypad_io_packet {
+    int pipe_fd;
+    Packet_queue* queue;
+} keypad_io_packet;
+
+void *keypad_io_thread(void* arg);
+void keypad_process();
 
 extern pid_t controller_pid;
-
-char numpad_symbols[] = {'1', '2', '3', 'A', '4', '5', '6', 'B', '7', '8', '9', 'C', '*', '0', '#', 'D'};//Symbols on the numpad
 
 unsigned char keypad_running = 1;
 
 pthread_mutex_t keypad_queue_lock;
 pthread_mutex_t keypad_queue_available;
 
-void *keypad_io_thread(void* arg);
-
-int readNumPad(){
-	/* This function reads the numpad and returns what button was pressed.
-	*  This function assumes that the numpad is configured with the rows as outputs and columns as inputs.
-	*  The columns are tied HIGH through an internal pull-up resistor, so if a column reads as low, then that row-column pair button was pressed
-	*/
-	int pressed = 0;//Used to keep track of how many buttons are pressed
-	int pressedNum = '-';//Value to return. -1 corresponds to an invalid input/no input
-	int rows[4] = {R1,R2,R3,R4};
-	int cols[4] = {C1,C2,C3,C4};
-	for(int i = 0; i < 4; i++){
-		digitalWrite(rows[i], LOW);//Make a row logic low
-		for(int j = 0; j < 4; j++){
-			if(digitalRead(cols[j]) == LOW){//if a column is a logic low, then the button on that row, column was pressed
-				pressed++;
-				pressedNum = 4*i + j;//Map the button to a number
-			}
-		}
-		digitalWrite(rows[i], HIGH);//Set the row back to high to not interfere with other rows
-	}
-	if(pressed > 1 || pressedNum == '-'){
-		return '-';//Return invalid if multiple buttons are pressed
-	}else{
-		return numpad_symbols[pressedNum];
-	}
-}
-
 // Debug print statements from this process are White (\033[0;m)
 void keypad_process(){
 
     KEYPAD_PRINTF("Keypad reader process launched\n");
 
-	wiringPiSetup();
-	pinMode(7, OUTPUT);//LED for Testing, on Pin GPIOG11
-	pinMode(R1, OUTPUT);
-	pinMode(R2, OUTPUT);
-	pinMode(R3, OUTPUT);
-	pinMode(R4, OUTPUT);
-	pinMode(C1, INPUT);
-	pinMode(C2, INPUT);
-	pinMode(C3, INPUT);
-	pinMode(C4, INPUT);
-	pullUpDnControl(C1, PUD_UP);//Set pull-up resistors
-	pullUpDnControl(C2, PUD_UP);
-	pullUpDnControl(C3, PUD_UP);
-	pullUpDnControl(C4, PUD_UP);
+    // Initialize HAL
+    if (hal_keypad_init() != 0) {
+        KEYPAD_PRINTF("Failed to initialize keypad HAL\n");
+        // We continue anyway, maybe it will work later or user will plug it in
+    } else {
+        KEYPAD_PRINTF("Keypad HAL initialized: %s\n", hal_keypad_get_impl_name());
+    }
 
     KEYPAD_PRINTF("Connecting to input/output pipes\n");
 
@@ -123,9 +143,16 @@ void keypad_process(){
         Inst_packet* received_packet = dequeue(input_queue);
         pthread_mutex_unlock(&keypad_queue_lock);
         pthread_mutex_unlock(&keypad_queue_available);
+        
         char read_value = -1;
         if (received_packet->data[0] == 'r') {
-            read_value = readNumPad();
+            // Use HAL to read keypad
+            KeypadEvent event = hal_keypad_read();
+            if (event.valid) {
+                read_value = event.key;
+            } else {
+                read_value = '-'; // No key pressed
+            }
         }
 
         Inst_packet* packet_to_send = create_inst_packet(KEYPAD, 1, (unsigned char*)&read_value, received_packet->tag);
@@ -139,6 +166,8 @@ void keypad_process(){
     destroy_queue(input_queue);
     close(input_pipe_fd);
     close(output_pipe_fd); //Graceful closing is always nice :)
+    
+    hal_keypad_cleanup();
     return;
 }
 
@@ -197,19 +226,8 @@ void *keypad_io_thread(void* arg) {
     return NULL;
 }
 
-void keypadTurnon(){
-    wiringPiSetup();
-	pinMode(7, OUTPUT);//LED for Testing, on Pin GPIOG11
-	pinMode(R1, OUTPUT);
-	pinMode(R2, OUTPUT);
-	pinMode(R3, OUTPUT);
-	pinMode(R4, OUTPUT);
-	pinMode(C1, INPUT);
-	pinMode(C2, INPUT);
-	pinMode(C3, INPUT);
-	pinMode(C4, INPUT);
-	pullUpDnControl(C1, PUD_UP);//Set pull-up resistors
-	pullUpDnControl(C2, PUD_UP);
-	pullUpDnControl(C3, PUD_UP);
-	pullUpDnControl(C4, PUD_UP);
-}
+#ifndef SHAREDLIB
+#include "keypad_firmware.c"
+#endif
+
+#endif
